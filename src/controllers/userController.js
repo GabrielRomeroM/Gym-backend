4a08acd7877a64c62f841c859c5b3a5b17a170d5
GabrielRomeroM <gabrielromeromaidana@gmail.com>
2025-01-26 10:27:49 -0300

    User correct errors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,12 +2,13 @@ import User from "../models/userModels.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+
+// Crear usuario
 export const createUser = async (req, res) => {
   try {
     const userData = new User(req.body);
     const { email } = userData;
 
-    // Validar que el email no sea repetido
     const userExist = await User.findOne({ email });
     if (userExist) {
       return res
@@ -15,7 +16,6 @@ export const createUser = async (req, res) => {
         .json({ message: `User with email: ${email} already exists` });
     }
 
-    // Guardar usuario en la base de datos
     await userData.save();
     res.status(201).json({ message: "User created", user: userData });
   } catch (error) {
@@ -23,31 +23,20 @@ export const createUser = async (req, res) => {
   }
 };
 
-// export const getUsers = async (req, res) => {
-//   try {
-//     const users = await User.find().populate("favoriteActivities");
-//     if (users.length === 0) {
-//       return res.status(204).json({ message: "There are no users" });
-//     }
-//     res.status(200).json(users);
-//   } catch (error) {
-//     return res.status(500).json({ message: "Internal server error", error });
-//   }
-// };
-
+// Obtiene todos los usuarios
 export const getUsers = async (req, res) => {
   try {
-    const users = await User.find(); // Obtiene todos los usuarios
+    const users = await User.find();
     if (users.length === 0) {
-      return res.status(204).json({ message: "There are no users" }); // Si no hay usuarios
+      return res.status(204).json({ message: "There are no users" }); 
     }
-    res.status(200).json(users); // Devuelve los usuarios encontrados
+    res.status(200).json(users);
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error }); // Manejo de errores
+    return res.status(500).json({ message: "Internal server error", error });
   }
 };
 
-
+// Borrar usuario
 export const deleteUser = async (req, res) => {
   try {
     const _id = req.params.id;
@@ -64,6 +53,7 @@ export const deleteUser = async (req, res) => {
   }
 };
 
+// Actualizar usuario
 export const updateUser = async (req, res) => {
   try {
     const _id = req.params.id;
@@ -81,6 +71,8 @@ export const updateUser = async (req, res) => {
   }
 };
 
+
+// Validar login
 export const validate = async (req, res) => {
   try {
     const { email, password } = req.body;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -11,19 +11,10 @@ import {
 const userRoute = express.Router();
 
 // Endpoints
-// Ruta de creación con POST
-userRoute.post("/create", createUser);
-
-// Ruta para obtener todos los usuarios con GET
-userRoute.get("/get", getUsers);
-
-// Ruta para eliminar un usuario por ID con DELETE
-userRoute.delete("/delete/:id", deleteUser);
-
-// Ruta para actualizar un usuario por ID con PUT
-userRoute.put("/update/:id", updateUser);
-
-// Ruta para validar login con POST
-userRoute.post("/login", validate);
+userRoute.post("/create", createUser); // Ruta de creación con POST
+userRoute.get("/get", getUsers); // Ruta para obtener todos los usuarios con GET
+userRoute.delete("/delete/:id", deleteUser); // Ruta para eliminar un usuario por ID con DELETE
+userRoute.put("/update/:id", updateUser); // Ruta para actualizar un usuario por ID con PUT
+userRoute.post("/login", validate); // Ruta para validar login con POST
 
 export default userRoute;
